3bf840e1e4f79b1e451c4d784aaae1883e189872
Athi Rajkumar <athithianr@gmail.com>
2021-06-04 23:47:49 -0400

    initial commit

---
+++ student-profiles/src/Student.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+const Student = ({ student }) => {
+    const [Grades, setGrades] = useState("");
+
+    const grades = student.grades.map((grade, index) => {
+        return <p>Test {index+1}: {grade}%</p>
+    }
+    );
+
+    const handleGradeDropdown = () =>
+    {
+        if(Grades == "")
+        {
+            setGrades(grades);
+        }
+        else {
+            setGrades("");
+        }
+    }
+
+    let sum = 0;
+    for (let i = 0; i < student.grades.length; i++) {
+        sum += parseInt(student.grades[i]);
+    }
+
+    return (
+        <div class="container">
+            <div class="left-half">
+                <img src={student.pic} id="circle" alt="BigCo Inc. logo" />
+            </div>
+            <div class="right-half">
+                <div className="temp">
+                    <h1>{student.firstName} {student.lastName}</h1>
+                    <p>Email: {student.email}</p>
+                    <p>Company: {student.company}</p>
+                    <p>Skill: {student.skill}</p>
+                    <p>Average: {sum / student.grades.length}%</p>
+                    <br></br>
+                    {Grades}
+                </div>
+            </div>
+            <div class="third">
+                <button onClick={handleGradeDropdown} class="expand-btn"><i className={`fa fa-${Grades === "" ? "plus" : "minus"}`}></i></button>
+            </div>
+        </div>
+
+    );
+}
+
+export default Student;
---
+++ student-profiles/src/StudentProfile.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Student from './Student';
+
+const StudentProfile = () => {
+
+    const callAPI = () => {
+        axios.get('https://www.hatchways.io/api/assessment/students')
+            .then(response => {
+                console.log(response.data.students);
+                setStudentList(response.data.students);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    const [studentList, setStudentList] = useState([]);
+    const [searchName, setSearchName] = useState("");
+
+
+    const renderedStudentList = studentList.filter((val) => {
+        if (searchName == "") {
+            return val;
+        }
+        else if (val.firstName.toLowerCase().includes(searchName.toLowerCase()) || val.lastName.toLowerCase().includes(searchName.toLowerCase())) {
+            return val;
+        }
+    }
+    ).map((student, index) => {
+        return <Student student={student}></Student>;
+    });
+    useEffect(() => {
+        callAPI();
+    }, []);
+    return (
+        <div className="student-list">
+            <input id="name-input" type="text" placeholder="Search By Name" onChange={(event) => {
+                setSearchName(event.target.value);
+            }} />
+            {renderedStudentList}
+        </div>
+    );
+}
+
+
+
+export default StudentProfile;
