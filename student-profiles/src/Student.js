909bb0366f6c159dd5fee4b5670a5f45ec9cfdfa
Athi Rajkumar <46330878+athithianr@users.noreply.github.com>
2021-06-06 01:13:50 -0400

    clean up ui

--- student-profiles/src/Student.js
+++ student-profiles/src/Student.js
@@ -1,34 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const Student = ({ student }) => {
+const Student = ({ student, renderItem}) => {
     const [Grades, setGrades] = useState("");
     const [Tags, setTags] = useState([]);
 
     const grades = student.grades.map((grade, index) => {
-        return <p class="grades">Test {index + 1}: {grade}%</p>
+        return <p className="grades">Test {index + 1}: {grade}%</p>
     },
     );
 
+    const tags = Tags.map((tag) => {
+        return <p style={{display: "inline", margin: "0 10px 0 10px", padding: "5px 5px 5px 5px", backgroundColor:'lightgray'}}>{tag}</p>
+    }
+    )
+
     const handleGradeDropdown = () => {
-        if (Grades == "") {
-            setGrades(grades);
-        }
-        else {
-            setGrades("");
-        }
+        (Grades === "") ? setGrades(grades) : setGrades("");
     }
 
     let sum = 0;
     for (let i = 0; i < student.grades.length; i++) {
         sum += parseInt(student.grades[i]);
     }
+    useEffect(() => {
+        renderItem(Tags, student.id);
+    }, [Tags]);
+
+    useEffect(() => {
+        if(student.hasOwnProperty('tags') && student.tags.length != 0)
+        {
+            console.log("Hello from " + student.firstName);
+            setTags(student.tags);
+        }
+    }, [student]);
+
 
     return (
-        <div class="container">
-            <div class="left-half">
-                <img src={student.pic} id="circle" alt="BigCo Inc. logo" />
+        <div className="container">
+            <div className="left-half">
+                <img src={student.pic} id="circle" alt="Student" />
             </div>
-            <div class="right-half">
+            <div className="right-half">
                 <div className="temp">
                     <h1>{student.firstName} {student.lastName}</h1>
                     <p>Email: {student.email}</p>
@@ -37,17 +49,19 @@ const Student = ({ student }) => {
                     <p>Average: {sum / student.grades.length}%</p>
                     <br></br>
                     {Grades}
-                    {Grades !== "" ? Tags : ""}
+                    <br></br>
+                    <div style={{ textAlign: 'left'}}>{Grades === "" ? "" : tags}</div>
+                    <br></br>
                     {Grades !== "" ? <input id="tag-input" type="text" placeholder="Add a tag" onKeyDown={(event) => {
                         if (event.key === 'Enter') {
-                            setTags(Tags.concat(event.target.value) + "  ")
+                            setTags(Tags.concat(event.target.value))
                             event.currentTarget.value = ""
                         }
                     }} /> : ""}
                 </div>
             </div>
-            <div class="third">
-                <button onClick={handleGradeDropdown} class="expand-btn"><i className={`fa fa-${Grades === "" ? "plus" : "minus"}`}></i></button>
+            <div className="third">
+                <button onClick={handleGradeDropdown} className="expand-btn"><i className={`fa fa-${Grades === "" ? "plus" : "minus"}`}></i></button>
             </div>
         </div>
 
--- student-profiles/src/StudentProfile.js
+++ student-profiles/src/StudentProfile.js
@@ -4,44 +4,69 @@ import Student from './Student';
 
 const StudentProfile = () => {
 
-    const callAPI = () => {
+    const populateStudents = () => {
         axios.get('https://www.hatchways.io/api/assessment/students')
             .then(response => {
                 console.log(response.data.students);
                 setStudentList(response.data.students);
             })
             .catch(error => {
-                console.log(error);
+                console.error(error);
+                setError("No Students were found!");
             })
     }
+
     const [studentList, setStudentList] = useState([]);
     const [searchName, setSearchName] = useState("");
+    const [Error, setError] = useState("");
+    const [TagName, setTagName] = useState("");
 
+    const renderItem = (x, id) =>
+    {
+        let array = studentList;
+        array[id-1].tags = x;
+        setStudentList(array);
+    }
 
     const renderedStudentList = studentList.filter((val) => {
-        if (searchName == "") {
+        if (searchName === "")
             return val;
-        }
-        else if (val.firstName.toLowerCase().includes(searchName.toLowerCase()) || val.lastName.toLowerCase().includes(searchName.toLowerCase())) {
+        else if (val.firstName.toLowerCase().includes(searchName.toLowerCase()) || val.lastName.toLowerCase().includes(searchName.toLowerCase()))
             return val;
+        else
+            return null;
+    }).filter((val) => {
+        if(TagName === "")
+            return val;
+        else if(val.tags && val.tags.length != 0) {
+            for(let i=0;i<val.tags.length;i++)
+            {
+                if(val.tags[i].toLowerCase().includes(TagName.toLowerCase()))
+                {
+                    return val;
+                }
+            }
         }
-    }
-    )
+    })
         .map((student, index) => {
-            return <Student student={student}></Student>;
+            return <Student student={student} renderItem={renderItem}></Student>;
         });
+
+        
     useEffect(() => {
-        callAPI();
+        populateStudents();
     }, []);
+
     return (
         <div className="student-list">
             <input id="name-input" type="text" placeholder="Search By Name" onChange={(event) => {
                 setSearchName(event.target.value);
             }} />
-            <input id="tag-input" type="text" placeholder="Search By Name" onChange={(event) => {
-                setSearchName(event.target.value);
+            <input id="tag-input" type="text" placeholder="Search By Tags" onChange={(event) => {
+                setTagName(event.target.value);
             }} />
             {renderedStudentList}
+            {Error}
         </div>
     );
 }
